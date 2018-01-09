bd6e9b7dd1cf4d7523dcfa059cc63d28def2b925
fernandofuentes <fernando.fuentes1113@gmail.com>
2018-01-08 21:19:47 -0600

    added sqls

---
+++ db/schema.sql
@@ -0,0 +1,9 @@
+CREATE database burgers_db;
+USE burgers_db;
+
+CREATE table burgers (
+id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+burger_name varchar(100) NOT NULL,
+devoured BOOLEAN DEFAULT false,
+date TIMESTAMP
+);
